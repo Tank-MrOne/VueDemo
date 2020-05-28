25bd134a682c2ab5c8c75e0aa85bafc446219072
Tank <wanjunqing@aliyun.com>
2020-05-28 16:05:48 +0800

    课程组件和登入功能实现

--- src/App.vue
+++ src/App.vue
@@ -1,98 +1,140 @@
 <template>
-    <div class="container">
-      <div class="alert alert-danger" role="alert">
-        <span>抱歉，您还未登入</span>
-        <div class="btn btn-primary btn-xs">登入</div>
-      </div>
-      <h2 class="page-header">VUE - 学习概况</h2>
-      <div class="btn btn-danger toIndex" @click="showComponents(0)">【 主页 】</div>
-      <div class="btn btn-primary toSingIn">【 上午 - 签到 】</div>
-      <div class="btn btn-primary toSingOut">【 下午 - 签到 】</div>
-      <div class="btn btn-warning toSingStat"  @click="showComponents(1)">【 签到统计 】</div>
-      <div class="btn btn-warning toCourseStat" @click="showComponents(2)">【 课程反馈统计 】</div>
-      <div class="btn btn-success toFeedStat" @click="showComponents(3)">【 意见反馈 】</div>
-      <div class="btn btn-info toAddCourse">【 添加小节 】</div>
-      <div class="contentMain">
-        <Page v-show="show[0]" :db="db"/>
-        <Course v-show="show[2]" :db="db"/>
-        <Singup v-show="show[1]" :db="db"/>
-        <Common v-show="show[3]" :db="db"/>
-      </div>
-      <div class="warpper-back">
-        <div class="warpper center">
-          <div class="showBack center"></div>
-          <div class="addCourseInput center col-md-8">
-            <h2 class="page-header text-center">添加课程小节</h2>
-            <form>
+  <div class="container">
+    <div v-if="line" class="alert alert-danger" role="alert">
+      <span>抱歉，您还未登入</span>
+      <div class="btn btn-primary btn-xs" @click="quit()">登入</div>
+    </div>
+    <div v-else class="alert alert-success" role="alert">
+      <span>
+        你好，
+        <b>{{db.user[online-1].name}}</b>,欢迎您的到来
+      </span>
+      <div class="btn btn-primary btn-xs" @click="quit()">切换</div>
+    </div>
+    <h2 class="page-header">VUE - 学习概况</h2>
+    <div class="btn btn-danger toIndex" @click="showComponents(0)">【 主页 】</div>
+    <div class="btn btn-primary toSingIn">【 签到 】</div>
+    <div class="btn btn-warning toSingStat" @click="showComponents(1)">【 签到统计 】</div>
+    <div class="btn btn-warning toCourseStat" @click="showComponents(2)">【 课程反馈统计 】</div>
+    <div class="btn btn-success toFeedStat" @click="showComponents(3)">【 意见反馈 】</div>
+    <div class="contentMain">
+      <Page v-show="show[0]" :db="db" />
+      <Course v-show="show[2]" :db="db" :cscs="changeStudentCourseStatus" />
+      <Singup v-show="show[1]" :db="db" />
+      <Common v-show="show[3]" :db="db" />
+    </div>
+    <div v-show="war" class="warpper-back">
+      <div class="warpper center">
+        <div class="showBack center"></div>
+        <div class="choose-identity">
+          <h2 class="page-header text-center">欢迎登入</h2>
+          <div class="center">
+            <div class="chooseSt">
               <div class="form-group">
-                <label for="exampleInputEmail1">在这输入课程小节标题</label>
-                <input type="text" class="form-control" placeholder="Course Title" />
-                <p class="h5"></p>
+                <label for="exampleInputPassword1">请输入用户编号</label>
+                <input v-model="online" type="text" class="form-control" placeholder="用户编号" />
               </div>
-              <button type="button" class="btn btn-block btn-info btn-default">发布</button>
-            </form>
-          </div>
-          <div class="choose-identity">
-            <h2 class="page-header text-center">你是老师还是学生？</h2>
-            <div class="center">
-              <div class="ts">
-                <div class="btn btn-primary btn-lg teacher">老师 Teacher</div>
-                <div class="btn btn-success btn-lg student">学生 Student</div>
-              </div>
-              <div class="chooseTs">
-                <div class="form-group">
-                  <label for="exampleInputPassword1">请输入通过密码</label>
-                  <input type="password" class="form-control" placeholder="Password" />
-                  <span></span>
-                </div>
-                <button type="button" class="btn btn-default">提交</button>
-                <button type="button" class="btn btn-info">跳过</button>
-              </div>
-              <div class="chooseSt">
-                <div class="form-group">
-                  <label for="exampleInputPassword1">请输入用户编号</label>
-                  <input type="text" class="form-control" placeholder="用户编号" />
-                  <span></span>
-                </div>
-                <button type="button" class="btn btn-default">提交</button>
-                <button type="button" class="btn btn-info">跳过</button>
-              </div>
+              <button type="button" class="btn btn-default" @click="signUp(online)">提交</button>
+              <button type="button" class="btn btn-info" @click="signUp()">跳过</button>
             </div>
           </div>
         </div>
       </div>
     </div>
+  </div>
 </template>
 
 
 
 <script type="text/ecmascript-6">
-import Page from './components/Page' ;
-import Course from './components/Course' ;
-import Singup from './components/Singup' ;
-import Common from './components/Common' ;
-import db from './public/js/db'
+import Page from "./components/Page";
+import Course from "./components/Course";
+import Singup from "./components/Singup";
+import Common from "./components/Common";
+import db from "./public/js/db";
 export default {
-    components:{
-        Page , Course , Singup , Common
+  components: {
+    Page,
+    Course,
+    Singup,
+    Common
+  },
+  data() {
+    return {
+      db: db,
+      online: "",
+      line: true,
+      war: true,
+      show: [true, false, false, false]
+    };
+  },
+  methods: {
+    showComponents(index) {
+      this.show = [false, false, false, false];
+      this.show[index] = true;
     },
-    data() {
-        return {
-            db : db,
-            show : [true,false,false,false]
+    changeStudentCourseStatus(x, index1, index2, uid) {
+      if (this.online == uid) {
+        if (x) {
+          this.db.section[index1].course[index2].good.push(uid);
+          let num = this.db.section[index1].course[index2].bad.indexOf(uid);
+          this.db.section[index1].course[index2].bad.splice(num, 1);
+        } else {
+          this.db.section[index1].course[index2].bad.push(uid);
+          let num = db.section[index1].course[index2].good.indexOf(uid);
+          this.db.section[index1].course[index2].good.splice(num, 1);
         }
-    },
-    methods: {
-      showComponents(index){
-        this.show = [false,false,false,false];
-        this.show[index] = true;
+      }else{
+        alert("抱歉，您只能对您个人的信息进行修改");
       }
     },
+    signUp(uid = 0) {
+      let flag = false;
+      for (let i = 0; i < this.db.user.length; i++) {
+        if (this.db.user[i].id == uid) {
+          flag = true;
+        }
+      }
+      if (flag) {
+        this.line = false;
+        this.war = false;
+      } else {
+        alert("输入编号有误，请重新输入");
+      }
+    },
+    quit() {
+      this.online = "";
+      this.line = true;
+      this.war = true;
+    }
+  }
 };
-
 </script>
 
 
 
 <style scoped>
+b {
+  font-weight: bold;
+  font-size: 18px;
+  color: deepskyblue;
+  letter-spacing: 2px;
+}
+.warpper-back {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  margin: auto;
+  width: 100%;
+  height: 100%;
+  z-index: 1;
+  background-color: rgba(0, 0, 0, 0.8);
+}
+.showBack {
+  line-height: 50px;
+  text-align: center;
+  font-size: 20px;
+}
 </style>
--- src/components/Course.vue
+++ src/components/Course.vue
@@ -9,21 +9,12 @@
           aria-expanded="false"
           class="btn btn-default btn-lg dropdown-toggle"
         >
-          <span class="time" style="margin-right: 20px;">2020-5-25</span>
+          <span class="time" style="margin-right: 20px;">{{date}}</span>
           <span class="caret"></span>
         </button>
         <ul class="dropdown-menu menu1">
-          <li>
-            <a href="javascript:;">2020-5-27</a>
-          </li>
-          <li>
-            <a href="javascript:;">2020-5-25</a>
-          </li>
-          <li>
-            <a href="javascript:;">2020-5-23</a>
-          </li>
-          <li>
-            <a href="javascript:;">2020-5-22</a>
+          <li v-for="(section, index) in db.section" :key="section.id">
+            <a href="javascript:;" @click="changeCourse(index)">{{section.date}}</a>
           </li>
         </ul>
       </div>
@@ -37,304 +28,26 @@
           aria-expanded="false"
           class="btn btn-default btn-info btn-lg dropdown-toggle"
         >
-          <span class="courseTitle">选择课程小节标题</span>
+          <span class="courseTitle">{{title}}</span>
           <span class="caret"></span>
         </button>
         <ul class="dropdown-menu menu2">
-          <li>
-            <a href="javascript:;">复习</a>
+          <li v-for="(course,index) in courses.course" :key="course.id">
+            <a href="javascript:;" @click="changeTitle(course,index)">{{course.title}}</a>
           </li>
-          <li>
-            <a href="javascript:;">列表的过滤和排序</a>
-          </li>
-          <li>
-            <a href="javascript:;">事件的绑定相关</a>
-          </li>
-          <li>
-            <a href="javascript:;">自动收集表单数据</a>
-          </li>
-          <li>
-            <a href="javascript:;">定时器的添加和清除</a>
-          </li>
-          <li>
-            <a href="javascript:;">生命周期回调（钩子）</a>
-          </li>
-          <li>
-            <a href="javascript:;">内置指令及自定义指令</a>
-          </li>
-          <li>
-            <a href="javascript:;">自定义过滤器处理数据</a>
-          </li>
-          <li>
-            <a href="javascript:;">自定义插件的使用</a>
-          </li>
-          <li>
-            <a href="javascript:;">过度和动画的操作</a>
-          </li>
+          
         </ul>
       </div>
     </div>
     <div class="col-md-12 tab5">
-      <h3 class="page-header">列表的过滤和排序</h3>
+      <h3 class="page-header">{{title}}</h3>
       <div class="container">
         <div class="row">
-          <div class="col-md-3 table-bordered text-center">
-            <div class="col-md-5 h4">吴鹏</div>
+          <div v-for="(user,index) in db.user" :key="user.id" class="col-md-3 table-bordered text-center">
+            <div class="col-md-5 h4">{{user.name}}</div>
             <div class="col-md-7 h4">
-              <button class="btn btn-xs goods">已掌握</button>
-              <button class="btn btn-xs bads">未掌握</button>
-            </div>
-          </div>
-          <div class="col-md-3 table-bordered text-center">
-            <div class="col-md-5 h4">刘钰</div>
-            <div class="col-md-7 h4">
-              <button class="btn btn-xs goods">已掌握</button>
-              <button class="btn btn-xs bads">未掌握</button>
-            </div>
-          </div>
-          <div class="col-md-3 table-bordered text-center">
-            <div class="col-md-5 h4">马禹涵</div>
-            <div class="col-md-7 h4">
-              <button class="btn btn-xs goods">已掌握</button>
-              <button class="btn btn-xs bads">未掌握</button>
-            </div>
-          </div>
-          <div class="col-md-3 table-bordered text-center">
-            <div class="col-md-5 h4">周田</div>
-            <div class="col-md-7 h4">
-              <button class="btn btn-xs goods">已掌握</button>
-              <button class="btn btn-xs bads">未掌握</button>
-            </div>
-          </div>
-          <div class="col-md-3 table-bordered text-center">
-            <div class="col-md-5 h4">周爱悦</div>
-            <div class="col-md-7 h4">
-              <button class="btn btn-xs goods">已掌握</button>
-              <button class="btn btn-xs bads">未掌握</button>
-            </div>
-          </div>
-          <div class="col-md-3 table-bordered text-center">
-            <div class="col-md-5 h4">李元元</div>
-            <div class="col-md-7 h4">
-              <button class="btn btn-xs goods">已掌握</button>
-              <button class="btn btn-xs bads">未掌握</button>
-            </div>
-          </div>
-          <div class="col-md-3 table-bordered text-center">
-            <div class="col-md-5 h4">高琪</div>
-            <div class="col-md-7 h4">
-              <button class="btn btn-xs goods">已掌握</button>
-              <button class="btn btn-xs bads">未掌握</button>
-            </div>
-          </div>
-          <div class="col-md-3 table-bordered text-center">
-            <div class="col-md-5 h4">郝继鹏</div>
-            <div class="col-md-7 h4">
-              <button class="btn btn-xs goods">已掌握</button>
-              <button class="btn btn-xs bads">未掌握</button>
-            </div>
-          </div>
-          <div class="col-md-3 table-bordered text-center">
-            <div class="col-md-5 h4">王寅虎</div>
-            <div class="col-md-7 h4">
-              <button class="btn btn-xs goods">已掌握</button>
-              <button class="btn btn-xs bads">未掌握</button>
-            </div>
-          </div>
-          <div class="col-md-3 table-bordered text-center">
-            <div class="col-md-5 h4">柴光成</div>
-            <div class="col-md-7 h4">
-              <button class="btn btn-xs goods">已掌握</button>
-              <button class="btn btn-xs bads">未掌握</button>
-            </div>
-          </div>
-          <div class="col-md-3 table-bordered text-center">
-            <div class="col-md-5 h4">陈天意</div>
-            <div class="col-md-7 h4">
-              <button class="btn btn-xs goods">已掌握</button>
-              <button class="btn btn-xs bads">未掌握</button>
-            </div>
-          </div>
-          <div class="col-md-3 table-bordered text-center">
-            <div class="col-md-5 h4">郭悦超</div>
-            <div class="col-md-7 h4">
-              <button class="btn btn-xs goods">已掌握</button>
-              <button class="btn btn-xs bads">未掌握</button>
-            </div>
-          </div>
-          <div class="col-md-3 table-bordered text-center">
-            <div class="col-md-5 h4">郝朝静</div>
-            <div class="col-md-7 h4">
-              <button class="btn btn-xs goods">已掌握</button>
-              <button class="btn btn-xs bads">未掌握</button>
-            </div>
-          </div>
-          <div class="col-md-3 table-bordered text-center">
-            <div class="col-md-5 h4">贾小芳</div>
-            <div class="col-md-7 h4">
-              <button class="btn btn-xs goods">已掌握</button>
-              <button class="btn btn-xs bads">未掌握</button>
-            </div>
-          </div>
-          <div class="col-md-3 table-bordered text-center">
-            <div class="col-md-5 h4">李昕</div>
-            <div class="col-md-7 h4">
-              <button class="btn btn-xs goods">已掌握</button>
-              <button class="btn btn-xs bads">未掌握</button>
-            </div>
-          </div>
-          <div class="col-md-3 table-bordered text-center">
-            <div class="col-md-5 h4">刘昶廷</div>
-            <div class="col-md-7 h4">
-              <button class="btn btn-xs goods">已掌握</button>
-              <button class="btn btn-xs bads">未掌握</button>
-            </div>
-          </div>
-          <div class="col-md-3 table-bordered text-center">
-            <div class="col-md-5 h4">刘雨鑫</div>
-            <div class="col-md-7 h4">
-              <button class="btn btn-xs goods">已掌握</button>
-              <button class="btn btn-xs bads">未掌握</button>
-            </div>
-          </div>
-          <div class="col-md-3 table-bordered text-center">
-            <div class="col-md-5 h4">马君琰</div>
-            <div class="col-md-7 h4">
-              <button class="btn btn-xs goods">已掌握</button>
-              <button class="btn btn-xs bads">未掌握</button>
-            </div>
-          </div>
-          <div class="col-md-3 table-bordered text-center">
-            <div class="col-md-5 h4">王凯凯</div>
-            <div class="col-md-7 h4">
-              <button class="btn btn-xs goods">已掌握</button>
-              <button class="btn btn-xs bads">未掌握</button>
-            </div>
-          </div>
-          <div class="col-md-3 table-bordered text-center">
-            <div class="col-md-5 h4">王文娟</div>
-            <div class="col-md-7 h4">
-              <button class="btn btn-xs goods">已掌握</button>
-              <button class="btn btn-xs bads">未掌握</button>
-            </div>
-          </div>
-          <div class="col-md-3 table-bordered text-center">
-            <div class="col-md-5 h4">杨阳</div>
-            <div class="col-md-7 h4">
-              <button class="btn btn-xs goods">已掌握</button>
-              <button class="btn btn-xs bads">未掌握</button>
-            </div>
-          </div>
-          <div class="col-md-3 table-bordered text-center">
-            <div class="col-md-5 h4">袁东昊</div>
-            <div class="col-md-7 h4">
-              <button class="btn btn-xs goods">已掌握</button>
-              <button class="btn btn-xs bads">未掌握</button>
-            </div>
-          </div>
-          <div class="col-md-3 table-bordered text-center">
-            <div class="col-md-5 h4">张亚欣</div>
-            <div class="col-md-7 h4">
-              <button class="btn btn-xs goods">已掌握</button>
-              <button class="btn btn-xs bads">未掌握</button>
-            </div>
-          </div>
-          <div class="col-md-3 table-bordered text-center">
-            <div class="col-md-5 h4">周涛</div>
-            <div class="col-md-7 h4">
-              <button class="btn btn-xs goods">已掌握</button>
-              <button class="btn btn-xs bads">未掌握</button>
-            </div>
-          </div>
-          <div class="col-md-3 table-bordered text-center">
-            <div class="col-md-5 h4">周泽生</div>
-            <div class="col-md-7 h4">
-              <button class="btn btn-xs goods">已掌握</button>
-              <button class="btn btn-xs bads">未掌握</button>
-            </div>
-          </div>
-          <div class="col-md-3 table-bordered text-center">
-            <div class="col-md-5 h4">庄俊强</div>
-            <div class="col-md-7 h4">
-              <button class="btn btn-xs goods">已掌握</button>
-              <button class="btn btn-xs bads">未掌握</button>
-            </div>
-          </div>
-          <div class="col-md-3 table-bordered text-center">
-            <div class="col-md-5 h4">李国林</div>
-            <div class="col-md-7 h4">
-              <button class="btn btn-xs goods">已掌握</button>
-              <button class="btn btn-xs bads">未掌握</button>
-            </div>
-          </div>
-          <div class="col-md-3 table-bordered text-center">
-            <div class="col-md-5 h4">龚欢</div>
-            <div class="col-md-7 h4">
-              <button class="btn btn-xs goods">已掌握</button>
-              <button class="btn btn-xs bads">未掌握</button>
-            </div>
-          </div>
-          <div class="col-md-3 table-bordered text-center">
-            <div class="col-md-5 h4">贺子文</div>
-            <div class="col-md-7 h4">
-              <button class="btn btn-xs goods">已掌握</button>
-              <button class="btn btn-xs bads">未掌握</button>
-            </div>
-          </div>
-          <div class="col-md-3 table-bordered text-center">
-            <div class="col-md-5 h4">胡强</div>
-            <div class="col-md-7 h4">
-              <button class="btn btn-xs goods">已掌握</button>
-              <button class="btn btn-xs bads">未掌握</button>
-            </div>
-          </div>
-          <div class="col-md-3 table-bordered text-center">
-            <div class="col-md-5 h4">霍利昂</div>
-            <div class="col-md-7 h4">
-              <button class="btn btn-xs goods">已掌握</button>
-              <button class="btn btn-xs bads">未掌握</button>
-            </div>
-          </div>
-          <div class="col-md-3 table-bordered text-center">
-            <div class="col-md-5 h4">刘香香</div>
-            <div class="col-md-7 h4">
-              <button class="btn btn-xs goods">已掌握</button>
-              <button class="btn btn-xs bads">未掌握</button>
-            </div>
-          </div>
-          <div class="col-md-3 table-bordered text-center">
-            <div class="col-md-5 h4">王艺琦</div>
-            <div class="col-md-7 h4">
-              <button class="btn btn-xs goods">已掌握</button>
-              <button class="btn btn-xs bads">未掌握</button>
-            </div>
-          </div>
-          <div class="col-md-3 table-bordered text-center">
-            <div class="col-md-5 h4">吴熙</div>
-            <div class="col-md-7 h4">
-              <button class="btn btn-xs goods">已掌握</button>
-              <button class="btn btn-xs bads">未掌握</button>
-            </div>
-          </div>
-          <div class="col-md-3 table-bordered text-center">
-            <div class="col-md-5 h4">张寅春</div>
-            <div class="col-md-7 h4">
-              <button class="btn btn-xs goods">已掌握</button>
-              <button class="btn btn-xs bads">未掌握</button>
-            </div>
-          </div>
-          <div class="col-md-3 table-bordered text-center">
-            <div class="col-md-5 h4">郑乔</div>
-            <div class="col-md-7 h4">
-              <button class="btn btn-xs goods">已掌握</button>
-              <button class="btn btn-xs bads">未掌握</button>
-            </div>
-          </div>
-          <div class="col-md-3 table-bordered text-center">
-            <div class="col-md-5 h4">万俊卿</div>
-            <div class="col-md-7 h4">
-              <button class="btn btn-xs goods">已掌握</button>
-              <button class="btn btn-xs bads">未掌握</button>
+              <button :class="chooseCssGood(user.id)" @click="cscs(true,cid,sid,user.id)">已掌握</button>
+              <button :class="chooseCssBad(user.id)" @click="cscs(false,cid,sid,user.id)">未掌握</button>
             </div>
           </div>
         </div>
@@ -346,7 +59,48 @@
 
 
 <script type="text/ecmascript-6">
-export default {};
+export default {
+  props:['db','cscs'],
+  data() {
+    return {
+      cid:this.db.section.length-1,
+      sid:this.db.section[this.db.section.length-1].course.length-1,
+      styleNor:"btn btn-xs",
+      styleCssGood:"btn btn-xs btn-success",
+      styleCssBad:"btn btn-xs btn-danger",
+      courses : this.db.section[this.db.section.length-1],
+      date  : this.db.section[this.db.section.length-1].date,
+      title : this.db.section[this.db.section.length-1].course[this.db.section[this.db.section.length-1].course.length-1].title,
+      studys : this.db.section[this.db.section.length-1].course[this.db.section[this.db.section.length-1].course.length-1]
+    }
+  },
+  methods: {
+    changeCourse(index){
+      console.log("----"+index);
+      
+      this.cid = index;
+      this.courses = this.db.section[index]
+      this.date = this.db.section[index].date
+    },
+    changeTitle(arr,index){
+      this.sid = index;
+      this.studys = arr
+      this.title = arr.title
+    },
+    chooseCssGood(uid){
+      if(this.studys.good.indexOf(uid)!== -1){
+        return this.styleCssGood
+      }
+      return this.styleNor
+    },
+    chooseCssBad(uid){
+      if(this.studys.bad.indexOf(uid)!== -1){
+        return this.styleCssBad
+      }
+      return this.styleNor
+    }
+  },
+};
 </script>
 
 
